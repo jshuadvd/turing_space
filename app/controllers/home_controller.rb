5dee7b4b5c9e66dc70d66a1a0d8e0a6419ef563f
Alex <alexxnavarretee@gmail.com>
2016-06-07 13:41:14 -0600

    changed variable name from service to apod

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    service = ApodServices.new
-    @picture_of_the_day = service.picture_of_the_day
+    apod = ApodServices.new
+    @picture_of_the_day = apod.picture_of_the_day
   end
 end
